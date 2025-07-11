2ca1221449249edcb109e360fa5975d3ca7225e5
vedang-sarvadhi <vedang@sarvadhi.com>
2025-07-11 09:54:49 +0530

    l

--- index.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementById('navbar-toggle');
-const navMenu = document.getElementById('nav-menu');
-
-toggleButton.addEventListener('click', () => {
-  navMenu.classList.toggle('active');
-});
